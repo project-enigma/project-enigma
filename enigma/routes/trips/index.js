a7ce8814e868315b1a0224e52cf59003509faa8d
Zeswen <37448151+Zeswen@users.noreply.github.com>
2018-11-26 18:50:58 +0100

    Feature/jln/trips section (#12)
    * Bugfixing
    
    * Updated trips dates, added custom functions on hbs and added cards
    
    * Added trips payment, success and whole routing

--- enigma/routes/trips/index.js
+++ enigma/routes/trips/index.js
@@ -1,14 +1,36 @@
 require('dotenv').config();
 
+const User = require('../../models/User');
+
+const Trip = require('../../models/Trip');
+
 const express = require('express');
 
 const tripsRouter = express.Router();
 
-const Trip = require('../../models/Trip');
 
 tripsRouter.get('/', (req, res, next) => {
   Trip.find()
     .then(trips => res.render('trips', { trips }))
+    .catch(err => next(err));
+});
+
+tripsRouter.get('/buy/:id', (req, res, next) => {
+  Trip.findById(req.params.id)
+    .then(trip => res.render('trips/buy', { trip }))
+    .catch(err => next(err));
+});
+
+
+tripsRouter.get('/buy/:id/success', (req, res, next) => {
+  Trip.findById(req.params.id)
+    .then(trip => res.render('trips/success', { trip }))
+    .catch(err => next(err));
+});
+
+tripsRouter.post('/buy/:id', (req, res, next) => {
+  Trip.findByIdAndUpdate(req.params.id, { $addToSet: { users: req.user.id } })
+    .then(() => res.redirect(`/trips/buy/${req.params.id}/success`))
     .catch(err => next(err));
 });
 
